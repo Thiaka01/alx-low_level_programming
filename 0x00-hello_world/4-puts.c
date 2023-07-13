1e968bc9634107711b287b2f6368e705e138dd70
Thiaka01 <thiakabrian@gmail.com>
2023-07-13 06:25:43 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
-int main() {
-     constanct char*message = "Programming is like building a multilingual puzzle";
-       puts(message);
-     return 0;
-       }   
+#include <stdio.h> 
+int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle");
+return 0;
+}
 
